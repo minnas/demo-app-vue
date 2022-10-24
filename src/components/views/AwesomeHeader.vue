6b46e40402130ce6ec5dda673ecbe6e6c8832535
Minna Salonen <minsalon@Minnas-MBP.home>
2022-10-24 11:23:09 +0300

    spinning header icon

--- src/components/views/AwesomeHeader.vue
+++ src/components/views/AwesomeHeader.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="awesome-header">
     <font-awesome-icon :icon="faSpider" size="lg" class="awesome-header-icon" />
-    <h1>One small Demo App</h1>
+    <div>
+      <h1>One small Demo App</h1>
+      <div class="bottom-line">(start/stop snowflake by click)</div>
+    </div>
     <font-awesome-icon
       :icon="faSnowflake"
       size="lg"
-      class="awesome-header-icon"
+      @click="toggleSpin"
+      :spin="spin"
+      class="awesome-header-icon spin"
     />
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { faSnowflake, faSpider } from "@fortawesome/free-solid-svg-icons";
 
 export default defineComponent({
@@ -18,9 +23,16 @@ export default defineComponent({
     title: String,
   },
   setup(props, { emit }) {
+    const spin = ref(true);
+    const toggleSpin = () => {
+      spin.value = !spin.value;
+    };
+
     return {
       faSpider,
       faSnowflake,
+      spin,
+      toggleSpin,
     };
   },
 });
@@ -34,8 +46,14 @@ export default defineComponent({
   grid-column-gap: 1rem;
   & .awesome-header-icon {
     color: rgba(148, 104, 254);
-    font-size: 2.6rem;
-    filter: drop-shadow(14px 15px 0px rgba(255, 255, 255, 0.4));
+    font-size: 3rem;
+    &:not(.spin) {
+      filter: drop-shadow(14px 15px 0px rgba(255, 255, 255, 0.4));
+      font-size: 2.6rem;
+    }
+    &.spin:hover {
+      cursor: pointer;
+    }
   }
   & h1 {
     font-size: 2.5rem;
@@ -43,6 +61,12 @@ export default defineComponent({
     margin: 0;
     padding: 5px 0;
     border-bottom: 4px dashed rgba(148, 104, 254, 0.6);
+  }
+  & .bottom-line {
+    text-align: center;
+    font-size: 1.2rem;
+    font-style: italic;
+    color: rgba(255, 255, 255, 0.5);
   }
 }
 </style>
